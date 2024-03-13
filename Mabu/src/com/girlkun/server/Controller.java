129322d54a365049194115cfed199841585fc63e
Thi Do Thai <ngocthiazaaaa12@gmail.com>
2024-03-13 21:34:47 +0700

    update

--- Mabu/src/com/girlkun/server/Controller.java
+++ Mabu/src/com/girlkun/server/Controller.java
@@ -68,8 +68,6 @@ public class Controller implements IMessageHandler {
             player = _session.player;
             byte cmd = _msg.command;
             if (cmd != -29 || cmd != -74) {
-                System.err.println("Player   " + player.name + "send message " + player.getCommandCount(cmd) + "cmd" + cmd);
-
                 // Check if the same command from the same player has been sent more than 1000 times in the last 5 seconds
                 // Check if it's time to reset the command count
                 if (st - player.getLastCommandTimestamp() >= THRESHOLD_INTERVAL) {
